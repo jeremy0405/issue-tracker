4b4ce447a1c44b8ea2e36f3f5170ef6f0175ed4a
hsjang <hsjang0405@gmail.com>
2022-06-14 13:50:56 +0900

    🔧  swagger config setting

---
+++ BE/src/main/java/team29/hoorry/issuetracker/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package team29.hoorry.issuetracker.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI().info(
+			new Info()
+				.title("Issue tracker API")
+				.description("Issue tracker API Document")
+				.version("1.0.0")
+		);
+	}
+
+}
