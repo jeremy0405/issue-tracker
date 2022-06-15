7e54afe9d5bdd3207a4d2ffecb2ca6e0055b1b63
who-hoo <who.ho3ov@gmail.com>
2022-06-16 02:49:04 +0900

    :sparkles: 도메인 클래스 작성 및 연관관계 매핑

---
+++ BE/src/main/java/team29/hoorry/issuetracker/core/issue/domain/CommentReaction.java
@@ -0,0 +1,34 @@
+package team29.hoorry.issuetracker.core.issue.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import team29.hoorry.issuetracker.core.member.domain.Member;
+
+@Entity
+public class CommentReaction {
+
+	@Id
+	@Column(name = "comment_reaction_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "comment_id", nullable = false)
+	private Comment comment;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Reaction reaction;
+}
