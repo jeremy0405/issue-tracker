1c32f453ee6bfe0d7634359f8da82ca73e5b016f
jm <jm.dev.study@gmail.com>
2022-06-15 23:21:15 +0900

    💄 로그인 페이지 구현
    Co-authored-by: mogooee <mogooee@users.noreply.github.com>

--- fe/src/pages/Login.tsx
+++ fe/src/pages/Login.tsx
@@ -1,5 +1,31 @@
+import { Link } from 'react-router-dom';
+
+import styled from 'styled-components';
+
+import LoginAndRegister from 'components/Organisms/LoginAndRegister';
+import { ReactComponent as Logo } from 'assets/logo/LogotypeLarge.svg';
+
+const StyledLogin = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  a {
+    margin-bottom: 61px;
+  }
+`;
+
 const Login = () => {
-  return <div>로그인 페이지</div>;
+  return (
+    <StyledLogin>
+      <Link to="/">
+        <Logo />
+      </Link>
+      <LoginAndRegister />
+    </StyledLogin>
+  );
 };
 
 export default Login;
