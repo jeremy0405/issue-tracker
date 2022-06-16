8b515a13b58b68351ff8566f7259105d06d39329
jm <jm.dev.study@gmail.com>
2022-06-17 03:43:06 +0900

    💄 Header 컴포넌트 구현

---
+++ fe/src/components/Organisms/Header/Header.stories.tsx
@@ -0,0 +1,9 @@
+import { ComponentMeta } from '@storybook/react';
+import Header from '.';
+
+export default {
+  title: 'Organisms/Header',
+  component: Header,
+} as ComponentMeta<typeof Header>;
+
+export const HeaderStory = <Header />;
