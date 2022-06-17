1b8dcd2dac1fdeb3e8ab85bbb54fdff4e03ec02e
연 <mogoo22@naver.com>
2022-06-17 14:19:59 +0900

    ✨ AddIssueForm Component

---
+++ fe/src/components/Molecules/AddIssueForm/AddIssueForm.stories.tsx
@@ -0,0 +1,20 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import AddIssueForm from 'components/Molecules/AddIssueForm';
+
+export default {
+  title: 'Molecules/AddIssueForm',
+  component: AddIssueForm,
+} as ComponentMeta<typeof AddIssueForm>;
+
+const Template: ComponentStory<typeof AddIssueForm> = (args) => <AddIssueForm {...args} />;
+
+export const Standard = Template.bind({});
+Standard.args = {
+  inputSize: 'LARGE',
+  inputType: 'text',
+  inputMaxLength: 15,
+  inputPlaceholder: '제목',
+  textareaSize: 'LARGE',
+  textareaMaxLength: 600,
+  textareaPlaceholder: '코멘트를 입력하세요',
+};
