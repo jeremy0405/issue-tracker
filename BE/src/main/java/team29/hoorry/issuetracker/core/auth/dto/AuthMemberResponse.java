010a77fd02b915132e1d1cc57104cb5229eef083
who-hoo <who.ho3ov@gmail.com>
2022-06-19 01:50:41 +0900

    ✨ oauth 인증 요청, 응답을 위한 DTO 작성
    Co-Authored-By: Jang HyungSuk (Jerry) <jeremy0405@naver.com>

---
+++ BE/src/main/java/team29/hoorry/issuetracker/core/auth/dto/AuthMemberResponse.java
@@ -0,0 +1,24 @@
+package team29.hoorry.issuetracker.core.auth.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthMemberResponse {
+
+	@JsonProperty("id")
+	private Long authId;
+
+	private String name;
+
+	private String email;
+
+	@JsonProperty("avatar_url")
+	private String profileImageUrl;
+
+}
