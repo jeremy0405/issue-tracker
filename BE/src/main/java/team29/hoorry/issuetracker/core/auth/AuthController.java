b6d69cee402c95f6126a5d99e73311d49ddc3cee
who-hoo <who.ho3ov@gmail.com>
2022-06-19 01:59:58 +0900

    ♻️ 엑세스 토큰 발급, 유저 정보 요청 로직 서비스 레이어로 분리
    Co-Authored-By: Jang HyungSuk (Jerry) <jeremy0405@naver.com>

--- BE/src/main/java/team29/hoorry/issuetracker/core/auth/AuthController.java
+++ BE/src/main/java/team29/hoorry/issuetracker/core/auth/AuthController.java
@@ -1,48 +1,27 @@
 package team29.hoorry.issuetracker.core.auth;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Collections;
-import org.springframework.http.MediaType;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
 import team29.hoorry.issuetracker.core.auth.dto.AuthMemberResponse;
-import team29.hoorry.issuetracker.core.auth.dto.AuthTokenRequest;
 import team29.hoorry.issuetracker.core.auth.dto.AuthToken;
 
 @RestController
 @RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
-	//client에서 깃허브 로그인 누르면 매칭되는 href
-	//https://github.com/login/oauth/authorize?client_id=f8e86d7a19896a7f2fd6&scope=user
+	private final AuthService authService;
 
 	@GetMapping
 	public ResponseEntity<AuthMemberResponse> publishToken(@RequestParam String code) {
-		AuthToken authToken = WebClient.create()
-			.post()
-			.uri("https://github.com/login/oauth/access_token")
-			.headers(header -> {
-				header.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-				header.setAcceptCharset(Collections.singletonList(StandardCharsets.UTF_8));
-			})
-			.bodyValue(AuthTokenRequest.of(code))
-			.retrieve()
-			.bodyToMono(AuthToken.class)
-			.block();
+		AuthToken authToken = authService.publishAuthToken(code);
+		AuthMemberResponse authMemberResponse = authService.getAuthUser(authToken);
 
-		AuthMemberResponse memberResponse = WebClient.create()
-			.get()
-			.uri("https://api.github.com/user")
-			.headers(header -> header.setBearerAuth(authToken.getAccessToken()))
-			.retrieve()
-			.bodyToMono(AuthMemberResponse.class)
-			.block();
-
-		return ResponseEntity.ok(memberResponse);
+		return ResponseEntity.ok(authMemberResponse);
 	}
 
 }
