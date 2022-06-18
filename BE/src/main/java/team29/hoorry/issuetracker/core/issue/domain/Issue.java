23d5efee8c78b37878c83bc0ecd682a89740989b
who-hoo <who.ho3ov@gmail.com>
2022-06-19 02:35:12 +0900

    ♻️ Embeddable로 구현한 엔티티 공통 속성 MappedSuperclass로 변경
    Co-Authored-By: Jang HyungSuk (Jerry) <jeremy0405@naver.com>

--- BE/src/main/java/team29/hoorry/issuetracker/core/issue/domain/Issue.java
+++ BE/src/main/java/team29/hoorry/issuetracker/core/issue/domain/Issue.java
@@ -4,24 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
-import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-import team29.hoorry.issuetracker.core.common.embeddable.DateTimeTracker;
+import team29.hoorry.issuetracker.core.common.BaseTimeEntity;
 import team29.hoorry.issuetracker.core.member.domain.Member;
 import team29.hoorry.issuetracker.core.milestone.domain.Milestone;
 
 @Entity
-public class Issue {
+public class Issue extends BaseTimeEntity {
 
 	@Id
 	@Column(name = "issue_id")
@@ -52,6 +50,4 @@ public class Issue {
 	@JoinColumn(name = "milestone_id")
 	private Milestone milestone;
 
-	@Embedded
-	private DateTimeTracker dateTimeTracker;
 }
