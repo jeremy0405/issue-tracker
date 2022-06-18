3c00f962e986a81426f8bae410bf30a15e091f1d
who-hoo <who.ho3ov@gmail.com>
2022-06-19 02:39:29 +0900

    🔥 불필요한 import문 제거
    Co-Authored-By: Jang HyungSuk (Jerry) <jeremy0405@naver.com>

--- BE/src/main/java/team29/hoorry/issuetracker/core/issue/dto/request/IssueUpdateRequest.java
+++ BE/src/main/java/team29/hoorry/issuetracker/core/issue/dto/request/IssueUpdateRequest.java
@@ -1,8 +1,6 @@
 package team29.hoorry.issuetracker.core.issue.dto.request;
 
 import java.util.List;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
