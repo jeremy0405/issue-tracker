319a1df7b6a7562c56f91bed01d2450304bc9da1
who-hoo <who.ho3ov@gmail.com>
2022-06-21 17:21:51 +0900

    ♻️ 커스텀 예외 수정
    - NullTokenException -> EmptyTokenException으로 이름 변경
    - MemberNotFoundException -> InvalidTokenException으로 이름 변경
    - EmptyTokenException과 InvalidTokenException의 수퍼 타입으로 AuthException 생성
    
    Co-Authored-By: Jang HyungSuk (Jerry) <jeremy0405@naver.com>

--- BE/src/main/java/team29/hoorry/issuetracker/core/exception/MemberNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package team29.hoorry.issuetracker.core.exception;
-
-public class MemberNotFoundException extends RuntimeException {
-
-	public MemberNotFoundException(String message) {
-		super(message);
-	}
-}
--- BE/src/main/java/team29/hoorry/issuetracker/core/exception/NullTokenException.java
+++
@@ -1,8 +0,0 @@
-package team29.hoorry.issuetracker.core.exception;
-
-public class NullTokenException extends RuntimeException {
-
-	public NullTokenException(String message) {
-		super(message);
-	}
-}
