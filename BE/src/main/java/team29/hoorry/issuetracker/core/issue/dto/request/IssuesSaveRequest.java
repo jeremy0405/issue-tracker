f6621e0017d95205a5cac0194022f000aa475d5d
who-hoo <who.ho3ov@gmail.com>
2022-06-22 16:34:44 +0900

    ✨ 이슈 등록 요청 DTO 작성
    Co-Authored-By: Jang HyungSuk (Jerry) <jeremy0405@naver.com>

--- BE/src/main/java/team29/hoorry/issuetracker/core/issue/dto/request/IssuesSaveRequest.java
+++ BE/src/main/java/team29/hoorry/issuetracker/core/issue/dto/request/IssuesSaveRequest.java
@@ -1,7 +1,25 @@
 package team29.hoorry.issuetracker.core.issue.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor
+@Schema(description = "이슈 등록 요청")
 public class IssuesSaveRequest {
 
+	@Schema(required = true)
+	private String title;
 
+	private String comment;
+
+	private List<Long> assigneeIds;
+
+	private List<Long> labelIds;
+
+	private Long milestoneId;
 
 }
