4d7fc85abeef89252d88b44d7e4c1f06f2883192
who-hoo <who.ho3ov@gmail.com>
2022-06-24 03:31:49 +0900

    🐛 어노테이션 누락으로 자동 주입 안되던 문제 해결
    Co-Authored-By: Jang HyungSuk (Jerry) <jeremy0405@naver.com>

--- BE/src/main/java/team29/hoorry/issuetracker/core/member/MemberRepository.java
+++ BE/src/main/java/team29/hoorry/issuetracker/core/member/MemberRepository.java
@@ -2,8 +2,10 @@ package team29.hoorry.issuetracker.core.member;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import team29.hoorry.issuetracker.core.member.domain.Member;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
 	Optional<Member> findByOauthId(Long oauthId);
