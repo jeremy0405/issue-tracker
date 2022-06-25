64ef6cc05d2c36d70734828e7d4b7ab4a821d8a3
who-hoo <who.ho3ov@gmail.com>
2022-06-25 18:24:46 +0900

    ✨ 라벨 등록 검증/예외 처리 추가

--- BE/src/main/java/team29/hoorry/issuetracker/core/label/dto/LabelAddRequest.java
+++ BE/src/main/java/team29/hoorry/issuetracker/core/label/dto/LabelAddRequest.java
@@ -1,6 +1,7 @@
 package team29.hoorry.issuetracker.core.label.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import javax.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import team29.hoorry.issuetracker.core.label.domain.Label;
@@ -11,12 +12,15 @@ import team29.hoorry.issuetracker.core.label.domain.Label;
 public class LabelAddRequest {
 
 	@Schema(required = true)
+	@NotBlank(message = "title은 필수 입력 값(공백 불가)입니다.")
 	private String title;
 
 	@Schema(required = true)
+	@NotBlank(message = "titleColor는 필수 입력 값(공백 불가)입니다.")
 	private String titleColor;
 
 	@Schema(required = true)
+	@NotBlank(message = "backgroundColor는 필수 입력 값(공백 불가)입니다.")
 	private String backgroundColor;
 
 	private String description;
