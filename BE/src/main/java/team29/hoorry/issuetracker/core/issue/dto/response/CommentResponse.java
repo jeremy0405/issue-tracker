4159dcf39cdf12a42675e36b749575d7e030aa77
who-hoo <who.ho3ov@gmail.com>
2022-06-26 03:27:19 +0900

    ♻️ 리액션별 멤버 그룹핑하는 로직 ReactionResponse -> CommentResponse로 이동
    Co-Authored-By: Jang HyungSuk (Jerry) <jeremy0405@naver.com>

--- BE/src/main/java/team29/hoorry/issuetracker/core/issue/dto/response/CommentResponse.java
+++ BE/src/main/java/team29/hoorry/issuetracker/core/issue/dto/response/CommentResponse.java
@@ -1,12 +1,16 @@
 package team29.hoorry.issuetracker.core.issue.dto.response;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import team29.hoorry.issuetracker.core.issue.domain.Comment;
 import team29.hoorry.issuetracker.core.issue.domain.CommentReaction;
+import team29.hoorry.issuetracker.core.member.domain.Member;
 import team29.hoorry.issuetracker.core.member.dto.MemberResponse;
 
 @Getter
@@ -18,15 +22,33 @@ public class CommentResponse {
 	private LocalDateTime createdAt;
 	private LocalDateTime updatedAt;
 	private String content;
-	private ReactionResponse reactions;
+	private List<ReactionResponse> reactions;
 
-	public static CommentResponse from(Comment comment, List<CommentReaction> reactions) {
+	public static CommentResponse from(Comment comment, List<CommentReaction> commentReactions) {
+		Map<String, List<Member>> membersGroupByReaction = groupingByReaction(commentReactions);
+
+		List<ReactionResponse> reactions = new ArrayList<>();
+		membersGroupByReaction.forEach((emoji, members) -> reactions.add(ReactionResponse.from(emoji, members)));
+
 		return new CommentResponse(
 			MemberResponse.from(comment.getWriter()),
 			comment.getCreatedAt(),
 			comment.getUpdatedAt(),
 			comment.getContent(),
-			ReactionResponse.from(reactions)
+			reactions
 		);
+	}
+
+	private static Map<String, List<Member>> groupingByReaction(List<CommentReaction> commentReactions) {
+		Map<String, List<Member>> membersGroupByReaction = new HashMap<>();
+		for (CommentReaction commentReaction : commentReactions) {
+			String emoji = commentReaction.getReaction().getEmoji();
+
+			if (!membersGroupByReaction.containsKey(emoji)) {
+				membersGroupByReaction.put(emoji, new ArrayList<>());
+			}
+			membersGroupByReaction.get(emoji).add(commentReaction.getMember());
+		}
+		return membersGroupByReaction;
 	}
 }
--- BE/src/main/java/team29/hoorry/issuetracker/core/issue/dto/response/ReactionResponse.java
+++ BE/src/main/java/team29/hoorry/issuetracker/core/issue/dto/response/ReactionResponse.java
@@ -1,13 +1,11 @@
 package team29.hoorry.issuetracker.core.issue.dto.response;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import team29.hoorry.issuetracker.core.issue.domain.CommentReaction;
+import team29.hoorry.issuetracker.core.member.domain.Member;
 import team29.hoorry.issuetracker.core.member.dto.MemberResponse;
 
 @Getter
@@ -15,30 +13,10 @@ import team29.hoorry.issuetracker.core.member.dto.MemberResponse;
 @AllArgsConstructor
 public class ReactionResponse {
 
-	@Getter
-	@NoArgsConstructor
-	@AllArgsConstructor
-	static class ReactionAndMember {
+	private String emoji;
+	private List<MemberResponse> members;
 
-		private String emoji;
-		private List<MemberResponse> members;
-	}
-
-	private List<ReactionAndMember> reaction = new ArrayList<>();
-
-	public static ReactionResponse from(List<CommentReaction> reactions) {
-		Map<String, List<MemberResponse>> test = new HashMap<>();
-		for (CommentReaction reaction : reactions) {
-			String emoji = reaction.getReaction().getEmoji();
-
-			if (!test.containsKey(emoji)) {
-				test.put(emoji, new ArrayList<>());
-			}
-			test.get(emoji).add(MemberResponse.from(reaction.getMember()));
-		}
-
-		List<ReactionAndMember> reaction = new ArrayList<>();
-		test.forEach((k, v) -> reaction.add(new ReactionAndMember(k, v)));
-		return new ReactionResponse(reaction);
+	public static ReactionResponse from(String emoji, List<Member> members) {
+		return new ReactionResponse(emoji, members.stream().map(MemberResponse::from).collect(Collectors.toList()));
 	}
 }
