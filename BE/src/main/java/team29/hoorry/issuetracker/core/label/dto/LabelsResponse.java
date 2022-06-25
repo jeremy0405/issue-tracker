af501a8ae69e57c40a326e97a63d66b2bec149f6
who-hoo <who.ho3ov@gmail.com>
2022-06-25 16:44:32 +0900

    ✨ 라벨 전체 조회 기능 구현

--- BE/src/main/java/team29/hoorry/issuetracker/core/label/dto/LabelsResponse.java
+++ BE/src/main/java/team29/hoorry/issuetracker/core/label/dto/LabelsResponse.java
@@ -4,12 +4,11 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import team29.hoorry.issuetracker.core.label.domain.Label;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class LabelsResponse {
 
-	private List<Label> labels;
+	private List<LabelResponse> labels;
 }
