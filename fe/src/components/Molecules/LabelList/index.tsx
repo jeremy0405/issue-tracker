acf597a0d07bac3986facb4ffa2014c5453301e0
jm <jm.dev.study@gmail.com>
2022-06-27 17:13:48 +0900

    💄✨ LabelList 컴포넌트 및 기능 구현 - 편집버튼 클릭시 수정 폼으로 변경

---
+++ fe/src/components/Molecules/LabelList/index.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+import { LabelTypes } from 'components/Atoms/Label';
+
+import CommonList from 'components/Molecules/CommonList/';
+import CommonTitle from 'components/Molecules/CommonList/CommonTitle';
+import LabelItem from 'components/Molecules/LabelList/LabelItem/';
+
+const StyledTitle = styled.span`
+  color: ${({ theme }) => theme.colors.label};
+  ${({ theme }) => theme.fontStyles.linkSmall};
+`;
+
+const LabelList = ({ labelData }: { labelData: LabelTypes[] }) => {
+  const labels = labelData.map((label) => {
+    return <LabelItem key={label.id} label={label} />;
+  });
+
+  return (
+    <CommonList>
+      <CommonTitle>
+        <StyledTitle>{labelData.length}개의 타이틀</StyledTitle>
+      </CommonTitle>
+      {labels}
+    </CommonList>
+  );
+};
+
+export default LabelList;
