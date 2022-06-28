54fd7e4c870afda2f219dc53e13fee4df2580773
연 <mogoo22@naver.com>
2022-06-28 23:47:17 +0900

    🔥 Header.stories.tsx 삭제
    Co-authored-by: 도비 <JiminKim-dev@users.noreply.github.com>

--- fe/src/components/Organisms/Header/Header.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentMeta } from '@storybook/react';
-import Header from 'components/Organisms/Header/';
-
-export default {
-  title: 'Organisms/Header',
-  component: Header,
-} as ComponentMeta<typeof Header>;
-
-export const HeaderStory = <Header />;
